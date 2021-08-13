ee4f2c2d2f507979f9315a93142f15cb3d99fef2
arash babaie <arash.babaie6@gmail.com>
2021-08-13 12:36:41 +0430

    convert home page to class component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { withTheme } from "styled-components";
+import React from "react";
 import { Text, Flex, Carousel } from "components";
 import { MediaDiscover } from "./home-page.style";
 import axios from "axiosHelper";
@@ -9,55 +8,58 @@ function generateMediaDiscoverSrc(listOfMovies) {
   return listOfMovies[randomNumber].poster_path;
 }
 
-const HomePage = () => {
-  const [mediaDiscoverSrc, setMediaDiscover] = useState(null);
-  const [fetchingPopular, setFetchingPopular] = useState(false);
-  const [popularData, setPopularData] = useState(null);
+class HomePage extends React.PureComponent {
+  state = {
+    mediaDiscoverSrc: null,
+    fetchingPopular: true,
+    popularData: null,
+  };
 
-  useEffect(() => {
-    setFetchingPopular(true);
+  componentDidMount = () => {
     axios()
       .get("/movie/popular")
       .then((res) => {
         const src = generateMediaDiscoverSrc(res.data.results);
-        setPopularData(res.data.results);
-        setMediaDiscover(src);
+        this.setState({ popularData: res.data.results, mediaDiscoverSrc: src });
       })
       .catch((err) => console.log({ err }))
-      .finally(() => setFetchingPopular(false));
-  }, []);
-  return (
-    <Flex width="100%" maxWidth="1300px" flexDirection="column">
-      <MediaDiscover
-        as={Flex}
-        width="100%"
-        flexDirection="column"
-        background="lightBlue"
-        padding="30px 40px"
-        position="relative"
-        height="360px"
-        mediaDiscoverSrc={mediaDiscoverSrc}
-        borderRadius="0 0 8px 8px"
-      >
-        <Text
+      .finally(() => this.setState({ fetchingPopular: false }));
+  };
+  render() {
+    const { mediaDiscoverSrc, fetchingPopular, popularData } = this.state;
+    return (
+      <Flex width="100%" maxWidth="1300px" flexDirection="column">
+        <MediaDiscover
+          as={Flex}
           width="100%"
-          fontSize="title1"
-          lineHeight="title1"
-          color="forceWhite"
+          flexDirection="column"
+          background="lightBlue"
+          padding="30px 40px"
+          position="relative"
+          height="360px"
+          mediaDiscoverSrc={mediaDiscoverSrc}
+          borderRadius="0 0 8px 8px"
         >
-          Explore in millions of movies!
-        </Text>
-      </MediaDiscover>
+          <Text
+            width="100%"
+            fontSize="title1"
+            lineHeight="title1"
+            color="forceWhite"
+          >
+            Explore in millions of movies!
+          </Text>
+        </MediaDiscover>
 
-      <Flex width="100%" padding="0 40px">
-        <Carousel
-          title="Popular movies"
-          data={popularData}
-          fetching={fetchingPopular}
-        />
+        <Flex width="100%" padding="0 40px">
+          <Carousel
+            title="Popular movies"
+            data={popularData}
+            fetching={fetchingPopular}
+          />
+        </Flex>
       </Flex>
-    </Flex>
-  );
-};
+    );
+  }
+}
 
-export default withTheme(HomePage);
+export default HomePage;
