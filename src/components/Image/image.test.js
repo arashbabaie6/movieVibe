311b7cae5d76b2a11da0aea06bdcbd1e5a5cd77a
arash babaie <arash.babaie6@gmail.com>
2021-08-14 21:30:44 +0430

    add testing system

---
+++ src/components/Image/image.test.js
@@ -0,0 +1,12 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from "react";
+import { screen, render, cleanup, fireEvent } from "@testing-library/react";
+
+describe("when implement testing", () => {
+  it("to be work!", () => {
+    expect(true).toBeTruthy();
+  });
+});
