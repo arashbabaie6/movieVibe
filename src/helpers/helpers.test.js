3cc291ea2873b0485c74910416599910800f939e
arash babaie <arash.babaie6@gmail.com>
2021-08-16 23:32:38 +0430

    revise asnyc test

--- src/helpers/helpers.test.js
+++ src/helpers/helpers.test.js
@@ -1,39 +1,15 @@
 import { searchMovies } from "./api.helper";
 
-describe("API:", () => {
-  const mockMovies = jest.fn(searchMovies);
-  const searchQuery = "bLack";
-  function handleCompare(item) {
-    return item.title.toLowerCase().includes(searchQuery.toLowerCase());
-  }
+const mockGet = jest.fn(() => "movies");
 
-  it("mock searchMovies testing resolve", () => {
-    mockMovies.mockResolvedValue({
-      data: { results: [{ title: "black", title: "black 2" }] },
-    });
+jest.mock("./axios.helper", () => () => ({
+  get: mockGet,
+}));
 
-    return mockMovies(searchQuery).then((res) => {
-      const assertion = res.data.results.every(handleCompare);
-      expect(assertion).toBeTruthy();
-    });
-  });
-
-  it("mock searchMovies testing reject", () => {
-    mockMovies.mockRejectedValue({
-      response: { data: { status_message: "not found", status_code: 404 } },
-    });
-
-    return mockMovies(searchQuery).catch((error) => {
-      expect.assertions(2);
-      expect(error.response.data.status_message).toBe("not found");
-      expect(error.response.data.status_code).toBe(404);
-    });
-  });
-
-  it("real api searchMovies testing", () => {
-    return searchMovies(searchQuery).then((res) => {
-      const assertion = res.data.results.every(handleCompare);
-      expect(assertion).toBeTruthy();
-    });
+describe("When called with searchQuery and page", () => {
+  it("Calls axios with correct URL and returns the result", () => {
+    expect(searchMovies("black", 2)).toBe("movies");
+    expect(mockGet).toHaveBeenCalledTimes(1);
+    expect(mockGet).toHaveBeenCalledWith("/search/movie?query=black&page=2");
   });
 });
